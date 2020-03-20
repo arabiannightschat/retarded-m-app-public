4502c86ef93abe6cdc096ee7517817de10496319
nightschat <jiachi1207@foxmail.com>
2020-03-20 15:09:47 +0800

    新增主题内容设计，新增图表功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    statusBarHeight:0,
+    statusBarHeight: 0,
+    lineChart: null
   },
 
   /**
@@ -13,13 +14,16 @@ Page({
    */
   onLoad: function (options) {
     var that = this
+    // 获取沉浸栏高度
     wx.getSystemInfo({
       success: function (res) {
         that.setData({
           statusBarHeight: res.statusBarHeight
         })
       },
-    })
+    });
+    // 绘制图表
+    this.createLineCharts();
   },
 
   /**
@@ -69,5 +73,86 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  /**
+   * 图表详细数据展示
+   */
+  touchHandler: function (e) {
+    this.data.lineChart.showToolTip(e, {
+      background: '#fe5856',
+      format: function (item, category) {
+        return category + ' ' + item.name + ' : ￥' + item.data
+      }
+    });
+  },
+
+  /**
+   * 获取图表数据方法
+   */
+  getChartsData: function () {
+    var categories = ['3-18', '3-19', '3-20', '3-21', '3-22', '3-23', '3-24',];
+    var data = [23, 40, 102, 13, 35, null, null];
+    var budget = [48.5, 48.5, 48.5, 48.5, 48.5, 48.5, 48.5];
+    return {
+      categories: categories,
+      data: data,
+      budget :budget
+    }
+  },
+
+  /**
+   * 创建曲线图
+   */
+  createLineCharts: function() {
+    var wxCharts = require('../../utils/wxcharts.js');
+    var windowWidth = 320;
+    try {
+      var res = wx.getSystemInfoSync();
+      windowWidth = res.windowWidth * 0.8
+    } catch (e) {
+      console.error('getSystemInfoSync failed!');
+    }
+
+    var chartsData = this.getChartsData();
+    this.data.lineChart = new wxCharts({
+      canvasId: 'lineCanvas',
+      type: 'line',
+      categories: chartsData.categories,
+      animation: true,
+      legend: true,
+      series: [{
+        name: '日实际消费',
+        data: chartsData.data,
+        format: function (val, name) {
+          return val.toFixed(2);
+        }
+      }, {
+        name: '日预算金额',
+          data: chartsData.budget,
+        format: function (val, name) {
+          return val.toFixed(2);
+        }
+      }],
+      xAxis: {
+        disableGrid: true,
+        fontColor :'#333'
+      },
+      yAxis: {
+        format: function (val) {
+          return val.toFixed(0);
+        },
+        min: 0,
+        fontColor: '#333',
+        gridColor :'#eee'
+      },
+      width: windowWidth,
+      height: 200,
+      dataLabel: false,
+      dataPointShape: true,
+      extra: {
+        lineStyle: 'curve'
+      }
+    });
   }
 })
